d2be3cd89a31242582736b531e8212c30efd6f7a
Marcelo Assis <marceleira@gmail.com>
2016-09-11 23:05:27 -0300

    Adicionando suporte a NFC ao aplicativo

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,5 +1,23 @@
 angular.module('SinasanApp.controllers.Main', [])
 
-.controller('MainController', function($scope){
-  
+.controller('MainController', function($scope, $nfc){
+
+        $scope.NFCcallback = function () {
+            alert("callback")
+        };
+
+        $scope.NFCSucesso = function () {
+            alert("sucesso")
+        };
+
+        $scope.NFCFalha = function () {
+            alert("falha")
+        };
+
+        $nfc.addTagDiscoveredListener($scope.NFCcallback, $scope.NFCSucesso, $scope.NFCFalha);
+
+        $scope.rastrear = function () {
+            $scope.isDisabled = true;
+        };
+
 });
