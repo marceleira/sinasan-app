8846e537f701eb6cdb633d7b18b007785d612636
Marcelo Assis <marceleira@gmail.com>
2016-09-13 00:46:02 -0300

    correcao da leitura do NFC

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,23 +1,27 @@
+
+function leituraNFC() {
+
+    nfc.addNdefListener(function(nfcEvent) {
+        var codigo;
+        if(nfcEvent.tag && nfcEvent.tag.ndefMessage && nfcEvent.tag.ndefMessage.length > 0) {
+            codigo = nfc.bytesToString(nfcEvent.tag.ndefMessage[0]["payload"]);
+            codigo = codigo.substr(3, codigo.length);
+            alert("codigo = "+codigo);
+        }
+    });
+
+};
+
 angular.module('SinasanApp.controllers.Main', [])
+.controller('MainController', function($scope){
 
-.controller('MainController', function($scope, $nfc){
+        document.addEventListener('deviceready', function(){
+            leituraNFC();
+        });
 
-        $scope.NFCcallback = function () {
-            alert("callback")
-        };
-
-        $scope.NFCSucesso = function () {
-            alert("sucesso")
-        };
-
-        $scope.NFCFalha = function () {
-            alert("falha")
-        };
-
-        $nfc.addTagDiscoveredListener($scope.NFCcallback, $scope.NFCSucesso, $scope.NFCFalha);
-
-        $scope.rastrear = function () {
-            $scope.isDisabled = true;
-        };
+        //$scope.rastrear = function () {
+        //    $scope.isDisabled = true;
+        //    leituraNFC();
+        //};
 
 });
