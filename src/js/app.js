01a51dfd13baf5c2dba30f52b35722a7987d9d1f
Marcelo Assis <marceleira@gmail.com>
2016-10-14 17:55:08 -0300

    Criando tela exibir detalhes da bolsa de sangue

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,11 @@ angular.module('SinasanApp', [
   'mobile-angular-ui',
   'SinasanApp.controllers.Main'
 ])
-
+.factory('config', function() {
+    //return {srv: "http://localhost:9090/sinasan-doacoes"};
+    return {srv: "http://sinasan.us.to"};
+})
 .config(function($routeProvider) {
   $routeProvider.when('/', {templateUrl:'home.html',  reloadOnSearch: false});
+  $routeProvider.when('/bolsa', {templateUrl:'bolsa.html',  reloadOnSearch: false});
 });
--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -13,15 +13,58 @@ function leituraNFC() {
 };
 
 angular.module('SinasanApp.controllers.Main', [])
-.controller('MainController', function($scope){
+.controller('MainController', function($scope, $http, $window, $location, $document, config){
 
-        document.addEventListener('deviceready', function(){
+        document.addEventListener('deviceready', function() {
             leituraNFC();
         });
 
-        //$scope.rastrear = function () {
-        //    $scope.isDisabled = true;
-        //    leituraNFC();
-        //};
+        // recupera dados da ultima pesquisa
+        $scope.dadosBolsa = $window.localStorage.getItem("dadosBolsa");
+        $scope.dadosBolsa = $scope.dadosBolsa ? JSON.parse($scope.dadosBolsa) : {};
+
+        $scope.isDisabled = false;
+        //$scope.consulta = null;
+
+        $scope.rastrear = function (codigoBolsa) {
+            $scope.isDisabled = true;
+            //leituraNFC();
+            $scope.ajaxRastreamento(codigoBolsa);
+            //$location.path("bolsa");
+        };
+
+        $scope.ajaxRastreamento = function(codigoBolsa) {
+
+            $http({
+                url: config.srv +'/ws/rastrearBolsa/'+codigoBolsa,
+                method: 'GET'
+            })
+            .error(function(dados) {
+                $scope.isDisabled = false;
+                alert("Verifique sua conexão com a internet");
+            })
+            .success(function(dados) {
+                if(dados.erro == undefined) {
+                    alert("Erro de comunicação com o servidor");
+                }
+                else if(dados.erro != 0) {
+                    alert(dados.msg_erro);
+                } else {
+                    $scope.dadosBolsa = dados;
+
+                    // salva os dados da ultima bolsa pesquisada
+                    $window.localStorage.setItem("dadosBolsa", JSON.stringify($scope.dadosBolsa));
+
+                    // mostra a tela de detalhes da bolsa
+                    $location.path("bolsa");
+                }
+
+                console.log(dados);
+
+                $scope.isDisabled = false;
+            });
+
+
+        };
 
 });
